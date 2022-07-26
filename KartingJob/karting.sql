08a1e3283d0c1a971ba3b9c832c3ff872293be3e
DeaDTriX <97814611+DeaDTriX1@users.noreply.github.com>
2022-07-26 17:22:29 +0200

    Update karting.sql

--- KartingJob/karting.sql
+++ KartingJob/karting.sql
@@ -1,19 +1,23 @@
 INSERT INTO `addon_account` (name, label, shared) VALUES
-    ('society_karting', 'Karting', 1)
+    ('society_Karting', 'Karting', 1)
+;
+
+INSERT INTO `addon_inventory` (name, label, shared) VALUES
+    ('society_Karting', 'Karting', 1)
 ;
 
 INSERT INTO `datastore` (name, label, shared) VALUES
-    ('society_karting', 'Karting', 1)
+    ('society_Karting', 'Karting', 1)
 ;
 
 INSERT INTO `jobs` (name, label) VALUES
-    ('karting', 'Karting')
+    ('Karting', 'Karting')
 ;
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-    ('karting',0,'recrue','Stagiaire',12,'{}','{}'),
-    ('karting',1,'novice','Employee',24,'{}','{}'),
-    ('karting',2,'experimente','Gérant piste',36,'{}','{}'),
-    ('karting',3,'chief','Manager',48,'{}','{}'),
-    ('karting',4,'boss','Boss',0,'{}','{}')
+    ('Karting',0,'recrue','Stagiaire',12,'{}','{}'),
+    ('Karting',1,'novice','Employee',24,'{}','{}'),
+    ('Karting',2,'experimente','Gérant piste',36,'{}','{}'),
+    ('Karting',3,'chief','Manager',48,'{}','{}'),
+    ('Karting',4,'boss','Boss',0,'{}','{}')
 ;
